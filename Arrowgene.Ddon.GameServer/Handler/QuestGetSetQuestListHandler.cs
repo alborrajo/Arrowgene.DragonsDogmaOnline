7d27323e84042a9c5ddd313b641dada30675a077
alborrajo <alvarolopezborr@gmail.com>
2024-01-19 21:37:54 +0100

    Minimal repro example of telclops quest

--- Arrowgene.Ddon.GameServer/Handler/QuestGetSetQuestListHandler.cs
+++ Arrowgene.Ddon.GameServer/Handler/QuestGetSetQuestListHandler.cs
@@ -1,9 +1,8 @@
-using System.Linq;
-using Arrowgene.Ddon.GameServer.Dump;
+using System.Collections.Generic;
 using Arrowgene.Ddon.Server;
 using Arrowgene.Ddon.Server.Network;
-using Arrowgene.Ddon.Shared.Entity;
 using Arrowgene.Ddon.Shared.Entity.PacketStructure;
+using Arrowgene.Ddon.Shared.Entity.Structure;
 using Arrowgene.Ddon.Shared.Network;
 using Arrowgene.Logging;
 
@@ -20,12 +19,47 @@ namespace Arrowgene.Ddon.GameServer.Handler
 
         public override void Handle(GameClient client, StructurePacket<C2SQuestGetSetQuestListReq> packet)
         {
-            S2CQuestGetSetQuestListRes pcap = EntitySerializer.Get<S2CQuestGetSetQuestListRes>().Read(GameFull.data_Dump_132);
-            // Only Knight's Bitter Enemy
-            pcap.SetQuestList = pcap.SetQuestList
-                .Where(setQuest => setQuest.Param.QuestId == 20005010)
-                .ToList();
-            client.Send(pcap);
+            //S2CQuestGetSetQuestListRes pcap = EntitySerializer.Get<S2CQuestGetSetQuestListRes>().Read(GameFull.data_Dump_132);
+            //var knightsBitterEnemy = pcap.SetQuestList
+            //    .Where(setQuest => setQuest.Param.QuestId == 20005010)
+            //    .Single();
+
+            S2CQuestGetSetQuestListRes res = new S2CQuestGetSetQuestListRes()
+            {
+                SetQuestList = new List<CDataSetQuestList>()
+                {
+                    new CDataSetQuestList()
+                    {
+                        Param = new CDataQuestList()
+                        {
+                            QuestScheduleId = 20005010,
+                            QuestId = 20005010,
+                            QuestProcessStateList = new List<CDataQuestProcessState>()
+                            {
+                                new CDataQuestProcessState()
+                                {
+                                    CheckCommandList = new List<CDataQuestProcessState.MtTypedArrayCDataQuestCommand>()
+                                    {
+                                        new CDataQuestProcessState.MtTypedArrayCDataQuestCommand()
+                                        {
+                                            ResultCommandList = new List<CDataQuestCommand>()
+                                            {
+                                                new CDataQuestCommand(68,  100, 26, -1)
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        },
+                        Detail = new CDataSetQuestDetail()
+                        {
+                            IsDiscovery = true
+                        }
+                    }
+                }
+            };
+
+            client.Send(res);
         }
     }
 }
